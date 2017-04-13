dd9a6e4e17d1cd22081d6c1e7d72edb9cbbf7162
Joseph Hawileh <hawilehj@gmail.com>
2017-04-12 22:53:09 -0700

    gravatar image sizes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,18 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "thumbnail img-responsive", width: "300", height: "300")
   end
+
+  def gravatar_small(user, options = { size: 40 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "img-responsive img-circle", width: "50", height: "50", style: "margin-right:8px; margin-top:-5px;")
+  end
+
+  def gravatar_mini(user, options = { size: 40 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "img-responsive img-circle", width: "35", height: "35", style: "margin-left:3px; margin-right:-5px;")
+  end
 end
